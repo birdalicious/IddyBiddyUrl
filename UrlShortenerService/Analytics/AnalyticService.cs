1d9e4e6d2ae7296007e9b2f6e34d1949cdf5514e
Jack Bird <jackbird050@gmail.com>
2023-08-29 21:58:43 +0100

    added basics to a analytics buffer

--- UrlShortenerService/Analytics/AnalyticService.cs
+++ UrlShortenerService/Analytics/AnalyticService.cs
@@ -1,30 +1,58 @@
 ﻿using MongoDB.Driver;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UrlShortenerService.Mappings;
+using System.Runtime.CompilerServices;
 
 namespace UrlShortenerService.Analytics
 {
     public class AnalyticService
     {
-        private IMongoCollection<Analytic> _analytics;
+        private IMongoCollection<Analytic> _analyticsCollection;
 
-        public AnalyticService(IMongoCollection<Analytic> analytics)
+        private readonly List<Analytic> _buffer = new();
+
+        public int BufferSize { get; init; } = 2;
+
+        public AnalyticService(IMongoCollection<Analytic> analytics, string writeConcernLevel = "1")
         {
-            _analytics = analytics.WithWriteConcern(WriteConcern.Unacknowledged);
+            _analyticsCollection = analytics.WithWriteConcern(MapWriteConcerLevel(writeConcernLevel));
         }
 
         public Task LogAccessAsync(string LinkId)
         {
-            return _analytics.InsertOneAsync(new Analytic
+            return AddToBuffer(new Analytic
             {
                 LinkId = LinkId,
                 LinkOwnerId = null,
                 DateTime = DateTimeOffset.UtcNow
             });
+        }
+        private WriteConcern MapWriteConcerLevel(string level)
+        {
+            if (level.ToLower() == "majority")
+            {
+                return WriteConcern.WMajority;
+            }
+
+            if (int.TryParse(level, out var count))
+            {
+                return new WriteConcern(Math.Max(count, 0));
+            }
+
+            return WriteConcern.WMajority;
+        }
+
+        private async Task AddToBuffer(Analytic analytic)
+        {
+            _buffer.Add(analytic);
+            if(_buffer.Count >= BufferSize) 
+            {
+                await FlushBuffer();
+            }
+        }
+
+        private async Task FlushBuffer()
+        {
+            await _analyticsCollection.InsertManyAsync(_buffer);
+            _buffer.Clear();
         }
     }
 }
