5285b56cc9246daba30aa988beed95194f2febff
Jack Bird <jackbird050@gmail.com>
2023-08-30 14:56:06 +0100

    Added the analytics buffer that is configurable

---
+++ UrlShortenerService/Analytics/AnalyticsBuffer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace UrlShortenerService.Analytics
+{
+    public class AnalyticsBuffer : IAsyncDisposable
+    {
+        public int BufferSize { get; init; }
+
+        private ConcurrentBag<Analytic> _buffer = new();
+
+        private IMongoCollection<Analytic> _analyticsCollection;
+
+        public AnalyticsBuffer(IMongoCollection<Analytic> analyticsCollection, string writeConcern = "majority", int bufferSize = 1) 
+        {
+            _analyticsCollection = analyticsCollection.WithWriteConcern(MapWriteConcerLevel(writeConcern));
+            BufferSize = bufferSize;
+        }
+
+        public async Task Add(Analytic analytic)
+        {
+            _buffer.Add(analytic);
+            if (_buffer.Count >= BufferSize)
+            {
+                await Flush();
+            }
+        }
+
+        public async ValueTask Flush()
+        {
+            await _analyticsCollection.InsertManyAsync(_buffer);
+            _buffer.Clear();
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            return Flush();
+        }
+        private WriteConcern MapWriteConcerLevel(string level)
+        {
+            if (level.ToLower() == "majority")
+            {
+                return WriteConcern.WMajority;
+            }
+
+            if (int.TryParse(level, out var count))
+            {
+                return new WriteConcern(Math.Max(count, 0));
+            }
+
+            return WriteConcern.WMajority;
+        }
+    }
+}
---
+++ UrlShortenerService/UrlShortenerServicesBuilder.cs
@@ -0,0 +1,66 @@
+﻿// Ignore Spelling: Mongo
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using System.Diagnostics;
+using UrlShortenerService.Analytics;
+using UrlShortenerService.Mappings;
+using UrlShortenerService.Routing;
+
+namespace UrlShortenerService
+{
+    public static class UrlShortenerServicesBuilder
+    {
+        public static IServiceCollection AddMongoDbClient(this IServiceCollection services)
+        {
+            return services.AddSingleton<IMongoClient, MongoClient>(s =>
+            {
+                var connectionString = s.GetRequiredService<IConfiguration>().GetConnectionString("MongoDb");
+                return new MongoClient(connectionString);
+            });
+        }
+        public static IServiceCollection AddMongoDbCollections(this IServiceCollection services)
+        {
+            services.AddScoped<IMongoCollection<Mapping>>(s => {
+                var c = s.GetRequiredService<IMongoClient>().GetDatabase("IddyBiddy").GetCollection<Mapping>("mappings");
+
+                c.Indexes.CreateOne(new CreateIndexModel<Mapping>("{ ShortLink: 1 }", new CreateIndexOptions { Unique = true }));
+                return c;
+            });
+
+            services.AddSingleton<IMongoCollection<Analytic>>(s =>
+                s.GetRequiredService<IMongoClient>().GetDatabase("IddyBiddy").GetCollection<Analytic>("analytics"));
+
+            return services;
+        }
+
+        public static IServiceCollection AddAnalyticsService(this IServiceCollection services)
+        {
+            services.AddSingleton<AnalyticsBuffer>(s =>
+            {
+                var bufferSize = 1;
+                int.TryParse(s.GetRequiredService<IConfiguration>().GetSection("Analytics")["BufferSize"], out bufferSize);
+                var writeConcern = s.GetRequiredService<IConfiguration>().GetSection("Analytics")["WriteConcern"] ?? "majority";
+                var collection = s.GetRequiredService<IMongoCollection<Analytic>>();
+                
+                return new AnalyticsBuffer(collection, writeConcern, bufferSize);
+            });
+
+            services.AddScoped<AnalyticService>();
+            return services;
+        }
+
+        public static IServiceCollection AddUrlShorteningServices(this IServiceCollection services)
+        {
+            services.AddMongoDbClient();
+            services.AddMongoDbCollections();
+
+            services.AddAnalyticsService();
+            services.AddScoped<RoutingService>();
+            services.AddScoped<MappingService>();
+
+            return services;
+        }
+    }
+}
